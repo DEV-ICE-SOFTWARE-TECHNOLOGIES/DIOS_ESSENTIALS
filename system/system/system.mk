66b99e8f10b8912ad428724512a3e6f323edaa88
Marius Kopp <mariuskopp517@gmail.com>
2023-03-17 17:22:32 +0100

    230317
    -Cleanup for Pixel Devices
    -Added missing VR Lib
    -Added missing Pixel Configs (some)

--- system/system/system.mk
+++
@@ -1,8 +0,0 @@
-# --------------------------------------------------------------------------------------------------
-# Copyright (C) 2022 DEV ICE TECHNOLOGIES
-# --------------------------------------------------------------------------------------------------
-
-# App Permissions
-PRODUCT_PACKAGES += \
-    privapp-permissions-google \
-    privapp-permissions-platform
